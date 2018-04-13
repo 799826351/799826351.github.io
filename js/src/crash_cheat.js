eb8af8751c05ef6f03b9b84c3f2090b36b3b8d45
荃 <799826351@qq.com>
2018-04-13 19:19:52 +0800

    Site updated: 2018-04-13 19:19:52

---
+++ js/src/crash_cheat.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = 'bilibili( ゜ -゜)つロ ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '( ゜ -゜)つロ干杯~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
